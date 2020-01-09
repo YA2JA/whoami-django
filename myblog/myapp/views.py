6d4e43b1e47f35f6d9698197c9a3eb910743b42d
Ya2ja <johnsmith@example.com>
2020-01-09 21:29:48 +0100

    start project

---
+++ myblog/myapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from random import choice
+
+def index(request):
+    base = "1234567890AZERTYUIOPQSDFGHJKLMWXCVBN?!:@azertyuiopqsdfghjklmwxcvbn."
+    word = "".join(choice(base) for i in range(15))
+    title = get_client_ip(request)
+    content  =  get_meta(request)
+    context = {
+        "title": title,
+        "word": word,
+        "content": content
+    }
+    return render(request, "myapp/index.html", context)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_meta(request):
+    text = ""
+    for i in request.META.keys():
+        if i == "HTTP_USER_AGEN":
+            text += "%s:%s\n"%(i,request.META.get(i).split(",")[-1])
+        else:
+            text += "%s:%s\n"%(i,request.META.get(i))
+    return text
+
+def six(request):
+    base = "1234567890"
+    word = "".join(choice(base) for i in range(15))
+    context = {
+        "title": word[0:5],
+        "word": word
+    }
+    return render(request, "myapp/index.html", context)
