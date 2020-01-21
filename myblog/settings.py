ee57fb91013ab8ad218d92325b1bff06c75f0c6d
Ya2ja <johnsmith@example.com>
2020-01-21 17:18:11 +0100

    static test

--- myblog/settings.py
+++ myblog/settings.py
@@ -16,7 +16,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG =  False
     import django_heroku
 else:
-    DEBUG = False
+    DEBUG = True
     
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
