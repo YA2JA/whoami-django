9306a53f27144035f38d4a256ddc79dbe921e679
Ya2ja <johnsmith@example.com>
2020-01-24 21:19:17 +0100

    history function

--- myapp/views.py
+++ myapp/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from .models import Last_User
 from .forms import IpForm
+from random import randint
 
 import json
 import re
 import requests
 
 
+
 def main(request):
     user = Client(request)
     context = {
-        "title": "whoami",
-        "user_ip": user.ip,
-        "system_about": ("OS: "+user.os, "Browser: "+user.browser),
-        "location":("Contry: "+user.location.get("countryName"), "City: "+user.location.get("city"))
+    "title": "whoami",
+    "user_ip": user.ip,
+    "system_about": ("OS: "+user.os, "Browser: "+user.browser),
+    "location":("Contry: "+user.location.get("countryName"), "City: "+user.location.get("city"))
     }
-
-    print(Last_User.objects.all())
-    if not user.ip in Last_User.objects.all() and not isinstance(user.ip, type(None)):
-        new_ip = Last_User()
-        new_ip.ip =  user.ip
-        new_ip.save()
-
+    add_to_history(user.ip)
     return render(request, "myapp/index.html", context)
 
+def add_to_history(user_ip, _id = 1):
+    if user_ip not in Last_User.objects.all() and isinstance(user_ip, str):
+        new_user = Last_User.objects.get(id = _id)
+        new_user.ip =  user_ip
+        new_user.save()
+        
 class Client():
     def __init__(self, request):
         self.META = request.META
@@ -32,7 +34,7 @@ class Client():
         self.ip = self._get_ip()
         self.location = self._about_ip()
 
-    def _get_os(self):
+    def _get_os(self)->str:
 
         mac_names = "Mac" or "Macintosh" or "Mac_PowerPC"
         linux_names = "Linux" or "X11"
@@ -55,7 +57,7 @@ class Client():
                 return os_name[i]
         return "unknown"
 
-    def _get_browser(self):
+    def _get_browser(self) -> str:
         browsers = ("Edge", "Opera", "OPR", "Chrome", "Trident","Firefox", "Safari")
         for i in browsers:
             if i in self.META['HTTP_USER_AGENT']:
@@ -64,12 +66,12 @@ class Client():
                 return i
         return "unknown"
 
-    def _get_ip(self):
-        return self.META.get('HTTP_X_FORWARDED_FOR') or None
+    def _get_ip(self) -> str:
+        return self.META.get('HTTP_X_FORWARDED_FOR') or "%s.%s.%s.%s"%(randint(0,255),randint(100,255), randint(1,255),randint(0,255))
 
-
-    def _about_ip(self):
-        if  False:#self.ip:
-            return json.loads(requests.get("http://api.db-ip.com/v2/free/%s"%self.ip).text)
+    def _about_ip(self) -> str:
+        responce = json.loads(requests.get("http://api.db-ip.com/v2/free/%s"%self.ip).text)
+        if  None != responce.get("countryName"):
+            return responce
         return {"countryName": "Not found", "city" : "Not found"}
         #also can be use https://ipapi.com/ip_api.php?ip=
