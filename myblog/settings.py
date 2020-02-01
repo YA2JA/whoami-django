498156551d86cee22d3418ad1fe7cd9e6997b26b
Ya2ja <johnsmith@example.com>
2020-02-01 22:18:50 +0100

    smol fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -39,8 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -113,20 +112,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
 
-
-
 # Activate Django-Heroku.
-if os.environ.get('ENV') == 'PRODUCTION': django_heroku.settings(locals())
+django_heroku.settings(locals())
