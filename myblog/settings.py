a84f4566b6cc35c505c2baabf7f2281772894b9a
Ya2ja <johnsmith@example.com>
2020-02-08 17:40:51 +0100

    trying

--- myblog/settings.py
+++ myblog/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-if DEBUG:
+if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     STATICFILES_DIRS = (
@@ -123,6 +123,6 @@ if DEBUG:
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-if os.environ.get('ENV') == 'PRODUCTION': 
+if not DEBUG: 
     import django_heroku
     django_heroku.settings(locals())
